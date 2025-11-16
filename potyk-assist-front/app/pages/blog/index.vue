7271178d378397ccee34f7d9d67db067716867d3
potykion <potykion@gmail.com>
2025-11-16 19:31:36 +0300

    feat: blog

---
+++ potyk-assist-front/app/pages/blog/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-container>
+    <v-list-item
+    title="Item"
+    subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit"
+  ></v-list-item>
+  </v-container>
+</template>
+
+<style scoped lang="sass">
+
+</style>
