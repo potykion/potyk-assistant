9892399bf6aa36282175444d56b4d57d437312b5
potykion <potykion@gmail.com>
2025-11-16 20:20:03 +0300

    feat: blog

--- potyk-assist-front/app/pages/blog/index.vue
+++ potyk-assist-front/app/pages/blog/index.vue
@@ -1,13 +1,32 @@
 <script setup lang="ts">
-const { data: home } = await useAsyncData(() => queryCollection('content').path('/').first())
+const { data: articles } = await useAsyncData(() =>
+  queryCollection('content').path('/blog').all()
+)
 
 useSeoMeta({
-  title: home.value?.title,
-  description: home.value?.description
+  title: 'Блог',
+  description: 'Список статей блога'
 })
 </script>
 
 <template>
-  <ContentRenderer v-if="home" :value="home" />
-  <div v-else>Home not found</div>
+  <div>
+    <h1>Блог</h1>
+
+    <div v-if="articles?.length">
+      <v-list>
+        <v-list-item
+          v-for="article in articles"
+          :key="article._id"
+          :to="article._path"
+          link
+          :title="article.title || article._path"
+        >
+        </v-list-item>
+      </v-list>
+    </div>
+    <div v-else>
+      Статей пока нет
+    </div>
+  </div>
 </template>
