cdd78d238bad4afae18679273b2afa3c0beafe88
potykion <potykion@gmail.com>
2025-12-10 17:16:46 +0300

    feat(vid): movies

--- potyk-assist-front/app/pages/vid.vue
+++ potyk-assist-front/app/pages/vid.vue
@@ -1,5 +1,21 @@
 <script setup lang="ts">
+interface Movie {
+  image: string
+  title: string
+  why: string
+  kinopoiskUrl: string
+  downloadUrl: string
+}
 
+const movies: Movie[] = [
+  {
+    image: 'https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/2b9b671e-4558-4d08-b114-4d6ac79f26dd/3840x',
+    title: 'Красивая работа',
+    why: 'сигма-муви по мнению <a href="https://t.me/rzhavuykholodez/95">ржавого холодца</a>',
+    kinopoiskUrl: 'https://www.kinopoisk.ru/film/119363/',
+    downloadUrl: 'https://rutracker.org/forum/viewtopic.php?t=3010430/'
+  }
+]
 </script>
 
 <template>
@@ -12,34 +28,24 @@
 
     <h3>Посмотреть позже</h3>
     <v-row>
-      <v-col cols="3">
+      <v-col v-for="movie in movies" :key="movie.title" cols="3">
         <v-card>
-          <v-img
-              src="https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/2b9b671e-4558-4d08-b114-4d6ac79f26dd/3840x"/>
+          <v-img :src="movie.image"/>
 
           <v-card-item>
-            <v-card-title>Красивая работа</v-card-title>
-
-
+            <v-card-title>{{ movie.title }}</v-card-title>
           </v-card-item>
 
-
           <v-card-text>
-            <b>Почему?:</b> сигма-муви по мнению <a href="https://t.me/rzhavuykholodez/95">ржавого холодца</a>
-
-
+            <b>Почему?:</b> <span v-html="movie.why"></span>
           </v-card-text>
 
           <v-card-actions>
-            <v-btn href="https://www.kinopoisk.ru/film/119363/">КП</v-btn>
-            <v-btn href="https://rutracker.org/forum/viewtopic.php?t=3010430/">Скачать</v-btn>
+            <v-btn :href="movie.kinopoiskUrl">КП</v-btn>
+            <v-btn :href="movie.downloadUrl">Скачать</v-btn>
           </v-card-actions>
         </v-card>
-
-
       </v-col>
-
-
     </v-row>
 
     <v-row>
