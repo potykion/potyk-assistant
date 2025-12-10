2a5e932d62b1e9c093e6f7f57aa13a50ff8d76b7
potykion <potykion@gmail.com>
2025-12-10 22:19:38 +0300

    feat(vid): movie db

--- migrations/10.py
+++ migrations/10.py
@@ -16,7 +16,11 @@ def migrate(cursor: sqlite3.Cursor):
             watch_url     TEXT,
             why           TEXT
         );
-
+        INSERT INTO movies (title, image, kinopoisk_url, download_url, watch_url, why)
+        VALUES ('Красивая работа',
+                'https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/2b9b671e-4558-4d08-b114-4d6ac79f26dd/3840x',
+                'https://www.kinopoisk.ru/film/119363/', 'https://rutracker.org/forum/viewtopic.php?t=3010430/', null,
+                'сигма-муви по мнению <a href="https://t.me/rzhavuykholodez/95">ржавого холодца</a>');
 
         """
     )
