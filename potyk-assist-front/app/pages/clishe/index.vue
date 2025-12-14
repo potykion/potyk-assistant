d248e50d80e20a2194c17038dbde9008efc5b5a0
potykion <potykion@gmail.com>
2025-12-14 12:24:28 +0300

    feat: clishe

---
+++ potyk-assist-front/app/pages/clishe/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container>
+    <h1><code>clishe</code></h1>
+
+    <h2>Скачать логи бека</h2>
+
+    <pre
+      class="bg-white pa-2"
+    ><code>scp ibs@10.0.87.108:/var/log/vms/am-2/agent_manager.log ./108-am-2.log</code></pre>
+  </v-container>
+</template>
