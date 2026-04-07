2fda43cdd0ce91206cd37c3240213cfc2ec8e26e
potykion <potykion@gmail.com>
2026-04-07 15:23:27 +0300

    feat: bb cli

---
+++ kys_in_rest/bb_cli/bb_cli.py
@@ -0,0 +1,57 @@
+from urllib.parse import urljoin
+
+import httpx
+
+
+class BBCli:
+    def __init__(
+        self,
+        email: str,
+        # https://id.atlassian.com/manage-profile/security/api-tokens
+        api_token: str,
+        org: str,
+        repo: str,
+        base_url="https://api.bitbucket.org",
+    ):
+        self.email = email
+        self.api_token = api_token
+        self.client = httpx.Client(auth=(self.email, self.api_token))
+        self.org = org
+        self.repo = repo
+        self.base_url = base_url
+
+    def __del__(self):
+        self.client.close()
+
+    def get_src(
+        self,
+        path_wo_slash: str,
+        branch="master",
+    ):
+        path = f"/2.0/repositories/{self.org}/{self.repo}/src/{branch}/{path_wo_slash}"
+        resp = self.client.get(urljoin(self.base_url, path))
+        return resp.text
+
+    def commit(
+        self,
+        # like ".gitignore"
+        path_wo_slash: str,
+        content: str,
+        msg: str | None = None,
+        branch="master",
+    ):
+        msg = msg or f"Updated {path_wo_slash}"
+
+        files = {
+            "message": (None, msg),
+            "branch": (None, branch),
+            f"/{path_wo_slash}": (
+                path_wo_slash,
+                content,
+                "application/x-www-form-urlencoded",
+            ),
+        }
+        path = f"/2.0/repositories/{self.org}/{self.repo}/src"
+
+        resp = self.client.post(urljoin(self.base_url, path), files=files)
+        return resp.json()
